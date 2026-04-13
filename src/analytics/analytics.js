1f23626e25ba8699851127da0f2f3fa9529808f0
Themugo <mugo.james27@gmail.com>
2026-04-13 23:04:37 +0300

    Update analytics.js

--- src/analytics/analytics.js
+++ src/analytics/analytics.js
@@ -1,28 +1,3 @@
-const KEY = "rigid_analytics";
-
-function getData() {
-  return JSON.parse(localStorage.getItem(KEY)) || {
-    views: {},
-    whatsapp: {},
-  };
-}
-
-function save(data) {
-  localStorage.setItem(KEY, JSON.stringify(data));
-}
-
-export function trackView(carId) {
-  const data = getData();
-  data.views[carId] = (data.views[carId] || 0) + 1;
-  save(data);
-}
-
 export function trackWhatsApp(carId) {
-  const data = getData();
-  data.whatsapp[carId] = (data.whatsapp[carId] || 0) + 1;
-  save(data);
-}
-
-export function getAnalytics() {
-  return getData();
+  console.log("WhatsApp clicked:", carId);
 }
