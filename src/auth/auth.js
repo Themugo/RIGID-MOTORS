74b6baff196c1ab0bb5adde98788b6cf2ffd44e1
Themugo <mugo.james27@gmail.com>
2026-04-13 23:51:49 +0300

    Update auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,11 +1,12 @@
 const KEY = "rigid_user";
 
-export function getUser() {
-  return JSON.parse(localStorage.getItem(KEY));
+export function login(name, role) {
+  const user = { name, role };
+  localStorage.setItem(KEY, JSON.stringify(user));
 }
 
-export function login(user) {
-  localStorage.setItem(KEY, JSON.stringify(user));
+export function getUser() {
+  return JSON.parse(localStorage.getItem(KEY));
 }
 
 export function logout() {
