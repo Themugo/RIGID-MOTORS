ba252719773fb8cf0773c8976d6d96283fe8f789
Themugo <mugo.james27@gmail.com>
2026-04-14 00:50:09 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
-import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import VehicleCard from "../components/VehicleCard";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-export default function Home() {
-  const vehicles = useQuery(api.vehicles.listVehicles);
-
-  if (!vehicles) return <div>Loading...</div>;
-
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>🚗 Rigid Motors Marketplace</h1>
-
-      {vehicles.map((v) => (
-        <VehicleCard key={v._id} vehicle={v} />
-      ))}
-    </div>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
