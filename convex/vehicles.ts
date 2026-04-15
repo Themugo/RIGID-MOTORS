738c1dc0f460622e98db4a1ce412bf85860fab54
Themugo <mugo.james27@gmail.com>
2026-04-15 13:42:51 +0300

    Refactor getVehicles to remove logging
    Removed console log from getVehicles function.

--- convex/vehicles.ts
+++ convex/vehicles.ts
@@ -2,8 +2,6 @@ import { query } from "./_generated/server";
 
 export const getVehicles = query({
   handler: async (ctx) => {
-    const data = await ctx.db.query("vehicles").collect();
-    console.log("VEHICLES FROM DB:", data);
-    return data;
+    return await ctx.db.query("vehicles").collect();
   },
 });
