2b9364036950d977a68e542524b4ae72976412ae
Themugo <mugo.james27@gmail.com>
2026-04-15 12:51:15 +0300

    Update import statement and format Convex client initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
+
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+const convex = new ConvexReactClient(
+  import.meta.env.VITE_CONVEX_URL
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
